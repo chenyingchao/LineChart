d6e16964b467f6897d71591b23da3962acfdae15
陈营超 <chenyingchao@EDZdeMacBook-Pro.local>
2017-05-03 17:13:18 +0800

    Initial Commit

---
+++ LineChartView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LineChartView
+//
+//  Created by 陈营超 on 2017/5/3.
+//  Copyright © 2017年 陈营超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LineChartView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LineChartView
+//
+//  Created by 陈营超 on 2017/5/3.
+//  Copyright © 2017年 陈营超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
